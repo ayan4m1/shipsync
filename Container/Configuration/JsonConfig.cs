48663859545f86ecc4f9d34631ca2a43d72271b3
ayan4m1 <andrew@bulletlogic.com>
2016-08-07 11:02:13 -0400

    add installations to config

--- Container/Configuration/JsonConfig.cs
+++ Container/Configuration/JsonConfig.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace ShipSync.Container.Configuration
 {
     public class JsonConfig
     {
-        public Dictionary<string, string> Dropbox;
         public int Version;
+        public Dictionary<string, string> Dropbox;
+        public List<Installation> Installations;
     }
 }
