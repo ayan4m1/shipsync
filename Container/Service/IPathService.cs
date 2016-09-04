6dc6bfcb9423ff1dd0d87edfe9ad2e1fd16528f1
ayan4m1 <andrew@bulletlogic.com>
2016-09-04 11:17:17 -0400

    add PathService

---
+++ Container/Service/IPathService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShipSync.Container.Entity;
+
+namespace ShipSync.Container.Service
+{
+    public interface IPathService
+    {
+        string RemotePath { get; }
+        string CreateRemoteSavePath(GameSave save);
+        string CreateRemoteShipPath(Ship ship);
+        string CleanName(string name);
+    }
+}
---
+++ Container/Service/PathService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using log4net;
+using log4net.Core;
+using ShipSync.Container.Entity;
+
+namespace ShipSync.Container.Service
+{
+    public class PathService : IPathService
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(PathService));
+
+        /// <summary>
+        /// All characters matching this pattern will be replaced with CleanReplacement
+        /// </summary>
+        private static readonly Regex CleanPattern = new Regex(@"\W");
+
+        /// <summary>
+        /// Will take the place of any odd characters
+        /// </summary>
+        private static readonly string CleanReplacement = "-";
+
+        /// <summary>
+        /// Internal identifier used to locate our files in the Dropbox filesystem
+        /// </summary>
+        private readonly string _appName;
+
+        public PathService()
+        {
+            try
+            {
+                var assemblyName = Assembly.GetExecutingAssembly().GetName().FullName;
+                _appName = assemblyName.Substring(assemblyName.IndexOf('.'));
+            }
+            catch (Exception ex)
+            {
+                Log.Error("PathService failed to find a root assembly name", ex);
+                throw;
+            }
+        }
+
+        public string RemotePath => _appName;
+
+        public string CreateRemoteShipPath(Ship ship)
+        {
+            return _appName + "Ships/" + ship.FileName;
+        }
+
+        public string CreateRemoteSavePath(GameSave save)
+        {
+            return _appName + "Saves/" + save?.SaveSource + ".json";
+        }
+
+        public string CleanName(string input)
+        {
+            return CleanPattern.Replace(input, CleanReplacement).ToLower();
+        }
+    }
+}
