4a71e12235583e540d041bf1c81692b35cd99c59
ayan4m1 <andrew@bulletlogic.com>
2017-04-16 14:43:18 -0400

    add dropbox config parser

---
+++ Container/Configuration/DropboxConfigModule.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Autofac;
+using log4net;
+using Newtonsoft.Json;
+
+namespace ShipSync.Container.Configuration
+{
+    public class DropboxConfigModule : Module
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(DropboxConfigModule));
+
+        private readonly string _configPath;
+
+        public DropboxConfigModule(string configPath)
+        {
+            _configPath = configPath;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            if (!File.Exists(_configPath))
+            {
+                var ex = new FileNotFoundException(_configPath);
+                Log.Error("Exception trying to load config", ex);
+                throw ex;
+            }
+
+            using (var reader = File.OpenText(_configPath))
+            {
+                Log.Debug("Attempting to deserialize JSON config");
+                var configObject = JsonConvert.DeserializeObject<DropboxConfig>(reader.ReadToEnd());
+                builder.Register(c => configObject);
+            }
+        }
+    }
+}
