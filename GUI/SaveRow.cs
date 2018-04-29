8d29b14de65c86c1b7c9a6aa4617e0e691fb44dc
ayan4m1 <andrew@bulletlogic.com>
2018-04-29 13:20:09 -0400

    add some comments

--- GUI/SaveRow.cs
+++ GUI/SaveRow.cs
@@ -4,8 +4,19 @@ namespace ShipSync.GUI
 {
     public class SaveRow
     {
+        /// <summary>
+        /// Whether or not the row is checked for export
+        /// </summary>
         public bool Checked { get; set; }
+
+        /// <summary>
+        /// Name of the game save
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Reference to the GameSave object
+        /// </summary>
         public GameSave Tag { get; set; }
     }
 }
